4007f9e27888f23f64dbea4fc652a154c694bc58
TCH <saidkhajtchoutchaiev@MacBook-Air-de-SAIDKHAJ.local>
2022-08-07 01:04:51 +0200

    amelioration visuelle page contact

--- pages/contact.php
+++ pages/contact.php
@@ -1,5 +1,5 @@
 
-                <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="fr">
   <head>
     <meta charset="UTF-8" />
@@ -67,124 +67,85 @@
       </nav>
     </section>
     <main>
-   
-      <section class="content">
-       
-       
-       
-      <h4>CONTACT</h4>
-                    <div>
-                        <ul>
-                            <h5>Pour m'écrire:</h5>
-                            <br/><br/><br/>
-                            <li>contact@bepag.fr<br/><br/><br/></li>
-                            <li><a href="mailto:contact@bepag.fr">Pour m'écrire avec votre messagerie<br/> cliquez sur ce lien !</a></li>
-                        </ul>
-                        <form method="post">
-                            <h1>Contactez-nous</h1>
-                            <div class="separation"></div>
-                            <div class="corps-formulaire">
-                                <div class="left">
-                                <div class="groupe">
-                                    <label>Votre Prénom</label>
-                                    <input type="text" name="prenom" autocomplete="off" />
-                                    <i class="fas fa-user"></i>
-                                </div>
-                                <div class="groupe">
-                                    <label>Votre adresse e-mail</label>
-                                    <input  type="email" name="email" required autocomplete="off" />
-                                    <i class="fas fa-envelope"></i>
-                                </div>
-                                <div class="groupe">
-                                    <label>Votre téléphone</label>
-                                    <input type="text" name="phone" autocomplete="off" />
-                                    <i class="fas fa-mobile"></i>
-                                </div>
-                                </div>
+        <section class="content">
+            <h4>CONTACT</h4>
+            <div>
+                <form method="post">
+                    <h1>Contactez-nous</h1>
+                    <div class="separation"></div>
+                    <div class="corps-formulaire">
+                        <div class="left">
+                        <div class="groupe">
+                            <label>Votre Prénom</label>
+                            <input type="text" name="prenom" autocomplete="off" />
+                            <i class="fas fa-user"></i>
+                        </div>
+                        <div class="groupe">
+                            <label>Votre adresse e-mail</label>
+                            <input  type="email" name="email" required autocomplete="off" />
+                            <i class="fas fa-envelope"></i>
+                        </div>
+                        <div class="groupe">
+                            <label>Votre téléphone</label>
+                            <input type="text" name="phone" autocomplete="off" />
+                            <i class="fas fa-mobile"></i>
+                        </div>
+                        </div>
 
-                                <div class="droite">
-                                <div class="groupe">
-                                    <label>Message</label>
-                                    <textarea name="message" required placeholder="Saisissez ici..."></textarea>
-                                </div>
-                                </div>
-                            </div>
-                            <div class="pied-formulaire" align="center">
-                                <button>Envoyer le message</button>
-                            </div>
-                        </form>
-
-<!-- 
-                        <form method="post">
-                            <label>Votre email</label>
-                            <input type="email" name="email" required>
+                        <div class="droite">
+                        <div class="groupe">
                             <label>Message</label>
-                            <textarea name="message" required></textarea>
-                            <input type="submit">
-                        </form>
-
- -->
-
-                        <?php
-                        if (isset($_POST['message'])) {
-                            $entete  = 'MIME-Version: 1.0' . "\r\n";
-                            $entete .= 'Content-type: text/html; charset=utf-8' . "\r\n";
-                            $entete .= 'From: contact@bepag.fr' . "\r\n";
-                            $entete .= 'Reply-to: ' . $_POST['email'];
-
-                            $message = '<h4>Formulaire de contact Bepag Digital </h4>
-                            <p>
-                            <b>Prenom : </b>' . htmlspecialchars($_POST['prenom']) . '</p>
-                            <b>Telephone : </b>' . htmlspecialchars($_POST['phone']) . '</p>
-                            <b>Email : </b>' . $_POST['email'] . '<br>
-                            <b>Message : </b>' . htmlspecialchars($_POST['message']) . '</p>';
-
-                            $retour = mail('kirova@live.fr', 'Envoi depuis page Contact', $message, $entete);
-                            if($retour)
-                                echo '<p>Votre message a bien été envoyé.</p>';
-                        }
-                        ?>
+                            <textarea name="message" required placeholder="Saisissez ici..."></textarea>
+                        </div>
+                        </div>
                     </div>
-    
-
-
-
-
-
-
-
-
-
-
-        
-        
-      
-        
-        <footer>
-          <div class="footerPart1">
-            <ul class="footerContact">
-              <p id="contact">CONTACT</p>
-              <li>
-                <i class="fa fa-envelope-o"></i>
-                <a href="mailto:contact@bepag.fr">
-                <span class="footerEmail">contact@bepag.fr</a></span>
-              </li>
-              <li><a href="https://goo.gl/maps/vzJRRPh58jEYDUZx9"><i class="fa fa-map-marker"></i><span class="footerEmail">Paris</a></span></li>
-              <li><a href="https://www.linkedin.com/in/mrtchou/"><i class='fab fa-linkedin'></i><span class="footerEmail">Linkedin</a></span></li>
-              <li><a href="https://github.com/mrtchou"><i class='fab fa-github'></i><span class="footerEmail">GitHub</a></span></li>
-            </ul>
-            <ul class="footerOther">
-              <p id="informationsLegales">Informations Légales</p>
-              <li><a href="./pages/mentionsLegale.html">Mentions Legales</a></li>
-              <li><a href="./pages/cgu.html">CGU</a></li>
-              <!-- <li><a href="https://www.linkedin.com/in/mrtchou/">Conditions générales d’utilisation</a></li>
-              <li><a href="https://www.linkedin.com/in/mrtchou/">Conditions générales de vente</a></li>
-             --></ul>
-          </div>
-          <a href="#"><p class="footerRemonter"><img src="../img/icons/fleche-haut.webp"/></p></a>
-          <p class="footerCopyright">© Copyright 2022<span class="nomSite">Bepag Digital</span><br/>All rights reserved</p>
-        </footer>
-      </section>
+                    <div class="pied-formulaire" align="center">
+                        <button>Envoyer le message</button>
+                    </div>
+                </form>
+                <?php
+                if (isset($_POST['message'])) {
+                    $entete  = 'MIME-Version: 1.0' . "\r\n";
+                    $entete .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+                    $entete .= 'From: contact@bepag.fr' . "\r\n";
+                    $entete .= 'Reply-to: ' . $_POST['email'];
+                    $message = '<h4>Formulaire de contact Bepag Digital </h4>
+                    <p>
+                    <b>Prenom : </b>' . htmlspecialchars($_POST['prenom']) . '</p>
+                    <b>Telephone : </b>' . htmlspecialchars($_POST['phone']) . '</p>
+                    <b>Email : </b>' . $_POST['email'] . '<br>
+                    <b>Message : </b>' . htmlspecialchars($_POST['message']) . '</p>';
+                    $retour = mail('kirova@live.fr', 'Envoi depuis page Contact', $message, $entete);
+                    if($retour)
+                        echo '<p>Votre message a bien été envoyé.</p>';
+                }
+                ?>
+            </div>
+            <footer>
+            <div class="footerPart1">
+                <ul class="footerContact">
+                <p id="contact">CONTACT</p>
+                <li>
+                    <i class="fa fa-envelope-o"></i>
+                    <a href="mailto:contact@bepag.fr">
+                    <span class="footerEmail">contact@bepag.fr</a></span>
+                </li>
+                <li><a href="https://goo.gl/maps/vzJRRPh58jEYDUZx9"><i class="fa fa-map-marker"></i><span class="footerEmail">Paris</a></span></li>
+                <li><a href="https://www.linkedin.com/in/mrtchou/"><i class='fab fa-linkedin'></i><span class="footerEmail">Linkedin</a></span></li>
+                <li><a href="https://github.com/mrtchou"><i class='fab fa-github'></i><span class="footerEmail">GitHub</a></span></li>
+                </ul>
+                <ul class="footerOther">
+                <p id="informationsLegales">Informations Légales</p>
+                <li><a href="./pages/mentionsLegale.html">Mentions Legales</a></li>
+                <li><a href="./pages/cgu.html">CGU</a></li>
+                <!-- <li><a href="https://www.linkedin.com/in/mrtchou/">Conditions générales d’utilisation</a></li>
+                <li><a href="https://www.linkedin.com/in/mrtchou/">Conditions générales de vente</a></li>
+                --></ul>
+            </div>
+            <a href="#"><p class="footerRemonter"><img src="../img/icons/fleche-haut.webp"/></p></a>
+            <p class="footerCopyright">© Copyright 2022<span class="nomSite">Bepag Digital</span><br/>All rights reserved</p>
+            </footer>
+        </section>
     </main>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/gsap.min.js"></script>
     <script src="./js/jquery.min.js" type="text/javascript" ></script>
